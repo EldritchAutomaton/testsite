000b843ca0969a1b50e7b70d4dcdc075ba72b603
EldritchAutomaton <heisslertech@gmail.com>
2019-07-11 13:07:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//  CODE FOR STALKERTRON PROJ
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+
+alert("Nice to meet you, " + firstName + " " + lastName + ".");
+alert("Your age is " + " " + age + ".");
+
+
+console.log("Nice to meet you, " + firstName + " " + lastName + ".");
+console.log("Your age is " + " " + age + ".");
+
+/* CODE FOR AGE CALCULATOR PROJ
+ var age = prompt("How old are you?");
+ alert("You are" + " " + age * 365.25 + " days old.");
+ */
